80b7e73b4ad0f05cf66f23f3461ef72cee070b42
Alex Reshta <alex.reshta@gmail.com>
2019-11-24 22:20:35 +0200

    daily backup

---
+++ ex_9_SCA/build/run_cmake_cppcheck.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+THIS_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+TMP_DIR="${THIS_DIR}/../tmp"
+SRC_DIR="${THIS_DIR}/../imp"
+PRJ_DIR="${THIS_DIR}/../"
+
+#cmake "${SRC_DIR}"  -DCMAKE_INSTALL_PREFIX="${PRJ_DIR}" -DCMAKE_VERBOSE_MAKEFILE="on" -B"${TMP_DIR}" --trace
+cmake "${SRC_DIR}"  -DCMAKE_INSTALL_PREFIX="${PRJ_DIR}" -DCMAKE_VERBOSE_MAKEFILE="on" -B"${TMP_DIR}" "-DCMAKE_CXX_CPPCHECK=/usr/bin/cppcheck;--output-file=../lint/result.txt"
+
+pushd "${TMP_DIR}"
+make
+make install
+popd
+
+
---
+++ ex_9_SCA/imp/lib_a/src/lib_a.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+void lib_a_out1();
+void lib_a_out2();
